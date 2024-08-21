1831ba45fa6e3cf9fc47fa37ca954e587c04bce9
Bazurogg <bazurogg@gmail.com>
2024-08-21 15:02:19 +0200

    JAVA -> Swing menu -> setup jpeg background restart clean file

--- src/fr/pompey/dev/afpa/vue/swing/Menu.java
+++ src/fr/pompey/dev/afpa/vue/swing/Menu.java
@@ -3,7 +3,7 @@ package fr.pompey.dev.afpa.vue.swing;
 import fr.pompey.dev.afpa.entity.Librarian;
 
 import javax.swing.*;
-import java.awt.*;
+
 
 public class Menu extends JFrame {
     private JPanel Home;
@@ -21,37 +21,22 @@ public class Menu extends JFrame {
         setTitle("Library Management System");
 
         // Set the size of the frame
-        setSize(800, 420);
+        setSize(800, 640);
 
         // the app turn off when the user close the window
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 
         // Settings for the TextArea of the home menu
         // Create a welcome message with the librarian's name
         String welcomeMessage = "Welcome to the Library Management System!\n\n" +
                 "Hello, " + librarian.getFirstname() + " " + librarian.getLastname() + "!\n" +
-                "Welcome to our library management system. Here you can manage books, users, and loans.";
-
-        // Create an ImagePanel with the background image
-        ImagePanel backgroundPanel = new ImagePanel("C:\\Users\\Fabulous\\Pictures\\affichemanga01.jpg");
+                "Here you can manage books, users, and book's rent.";
 
         // set background color
         welcomeTextArea.setOpaque(false);
 
-        backgroundPanel.setLayout(new BorderLayout());
-
         // set own text
         welcomeTextArea.setText(welcomeMessage);
-
-        // Add the welcomeTextArea to the background panel
-        backgroundPanel.add(welcomeTextArea, BorderLayout.CENTER);
-
-        // Add the background panel to the Home panel
-        setContentPane(backgroundPanel);
-
-        // Make the frame visible
-        setVisible(true);
 
     }
 
