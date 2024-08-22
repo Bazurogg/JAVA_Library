2cf5d8491718a31a6816f2e685e4dcf43a48052b
Bazurogg <bazurogg@gmail.com>
2024-08-22 16:56:04 +0200

    JAVA -> Swing -> UserTableModel -> setup file

--- src/fr/pompey/dev/afpa/tablemodel/UserTableModel.java
+++ src/fr/pompey/dev/afpa/tablemodel/UserTableModel.java
@@ -1,4 +1,29 @@
 package fr.pompey.dev.afpa.tablemodel;
 
-public class UserTableModel {
+
+import fr.pompey.dev.afpa.entity.User;
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class UserTableModel extends AbstractTableModel {
+
+    private final String[] columnNames =  {"Firstname", "Lastname", "Email", "Registration Date"};
+
+    private List<User> users;
+
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+
 }
