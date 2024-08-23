555db0e6a0ec59aef593fe23e75ee8aafadbcc88
Bazurogg <bazurogg@gmail.com>
2024-08-23 11:48:58 +0200

    JAVA -> Swing -> User list table -> + icon

---
+++ src/fr/pompey/dev/afpa/vue/swing/AddUser.java
@@ -0,0 +1,11 @@
+package fr.pompey.dev.afpa.vue.swing;
+
+import javax.swing.*;
+
+public class AddUser {
+    private JTextField firstnameField;
+    private JTextField lastnameField;
+    private JTextField emailField;
+    private JButton cancelButton;
+    private JButton addButton1;
+}
