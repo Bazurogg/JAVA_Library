9e733ff666372ed2279cf3815aee70fef1fe818b
Bazurogg <bazurogg@gmail.com>
2024-08-27 16:55:02 +0200

    JAVA -> Swing -> add Book frame -> resizing

--- src/fr/pompey/dev/afpa/vue/swing/AddBook.java
+++ src/fr/pompey/dev/afpa/vue/swing/AddBook.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddBook extends JFrame {
-    private final Library library;
+    private Library library;
     private JPanel AddBook;
     private JTextField titleField;
     private JTextField authorField;
